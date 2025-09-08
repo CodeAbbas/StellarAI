4efb8aa79e9fe51ef554800efd8ca6eab0362c69
Abbas Uddin <abbax.uddin@gmail.com>
2025-09-08 15:54:24 +0100

    Api endpoint updated and fetch call corrected to include method specification.

--- api/generate.js
+++ api/generate.js
@@ -11,7 +11,7 @@ export default async function handler(request, response) {
 
   const API_KEY = process.env.API_KEY;
 
-  const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/imagen-3.0-generate-005:predict?key=${API_KEY}`;
+  const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/imagen-3.0-generate-002:predict?key=${API_KEY}`;
 
   const payload = {
     instances: [{ prompt: prompt }],
