54987fe54e94214a5f4df9f863332bb1552f74a7
Abbas Uddin <abbax.uddin@gmail.com>
2025-09-08 17:47:39 +0100

    Download button adjusted to be a text button instead of an icon for better usability.

--- src/main.js
+++ src/main.js
@@ -104,8 +104,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
             // download button
             const downloadBtn = document.createElement('button');
-            downloadBtn.className = 'download-btn';
-            downloadBtn.innerHTML = `<svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"></path></svg>`;
+            downloadBtn.className = 'download-btn text-[10px] font-semibold h-4 w-14 rounded-sm';
+            downloadBtn.textContent = 'Download';
 
 
             downloadBtn.onclick = (e) => {
